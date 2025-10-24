d9dde29eca44222c1a4ed50724199d8f7fb1c647
Xichen Zhou <sichem.zh@gmail.com>
2025-10-24 15:48:52 -0400

    introducing singleton
    Signed-off-by: Xichen Zhou <sichem.zh@gmail.com>

---
+++ include/ebus/singleton.hh
@@ -0,0 +1,28 @@
+#pragma once
+#include "export.hh"
+
+#include <mutex>
+
+namespace EBUS_NS
+{
+
+template <typename T>
+class singleton
+{
+public:
+    static T& get_instance()
+    {
+        // as simple as possible, the instance itself should be protected.
+        static T s_instance = {};
+        return s_instance;
+    }
+
+protected:
+    singleton()  = default;
+    ~singleton() = default;
+
+    singleton(const singleton&) = delete;
+    singleton& operator=(const singleton&) = delete;
+};
+
+} // namespace EBUS_NS
